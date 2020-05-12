a60f208213f707875e2101e1b0d75d485d2d2a72
Juan D. Jara <juanorigami@gmail.com>
2020-05-12 02:32:59 +0200

    decode file name

--- api/storage.js
+++ api/storage.js
@@ -3,8 +3,8 @@ const fs = require('fs').promises
 
 function getVideoName (job, extension = '.webm') {
   const name = path.basename(job.data.url)
-  const ext = path.extname(job.data.url)
-  return name.replace(ext, extension)
+  const ext = path.extname(job.data.url)  
+  return decodeURIComponent(name.replace(ext, extension))
 }
 
 function deleteVideo (job) {
